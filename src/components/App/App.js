d73fe1a36b57c093803108bfb7ca4b50af6a275c
Grogswiller <ryanchipeniuk@gmail.com>
2022-02-02 19:49:57 -0700

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import BusinessList from './src/components/BusinessList';
+import SearchBar from './src/components/SearchBar/';
+
+function App() {
+  return (
+  <div class="App">
+    <h1>ravenous</h1>
+    <SearchBar />
+    <BusinessList /> 
+  </div>
+  )}
+
+export default App;
