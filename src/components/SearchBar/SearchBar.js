1a97cc736dbd3e47f933b9afa3fa32cfb78ba41b
Grogswiller <ryanchipeniuk@gmail.com>
2022-02-08 11:39:22 -0700

    searchBar correction, possible json fix

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -11,7 +11,7 @@ class SearchBar extends React.Component {
     renderSortByOptions() {
         return Object.keys(sortByOptions).map(sortByOption => {
             let sortByOptionValue = sortByOptions[sortByOption];
-            return <li key={sortByOptionValue}>{sortByOption}</li>
+            return (<li key={sortByOptionValue}>{sortByOption}</li>)
         });
     }
 
@@ -20,14 +20,14 @@ render() {
         <div className="SearchBar">
             <div className="SearchBar-sort-options">
                 <ul>
-                {renderSortByOptions}    
+                {this.renderSortByOptions()}    
                 </ul>
             </div>
-            <div class="SearchBar-fields">
+            <div className="SearchBar-fields">
                 <input placeholder="Search Businesses" />
                 <input placeholder="Where?" />
             </div>
-            <div class="SearchBar-submit">
+            <div className="SearchBar-submit">
                 <a>Let's Go</a>
             </div>
         </div>
