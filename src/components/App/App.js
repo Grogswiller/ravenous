326d1a9b7927845477c1d7413cf10defe297fa56
Grogswiller <ryanchipeniuk@gmail.com>
2022-02-14 12:32:33 -0700

    step 3 done, clickOn sort options not working

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
-let business = {
+const business = {
   imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
   name: 'MarginOtto Pizzeria',
   address: '1010 Paddington Way',
@@ -24,14 +24,20 @@ const businesses = [
   business
 ];
 
-function App() {
+class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  }
+
+  render() {
     return (
-        <div className="App">
-          <h1>ravenous</h1>
-          <SearchBar />
-          <BusinessList businesses={businesses} /> 
-        </div>
-    )
-}  
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={businesses} />
+      </div>
+    );
+  }
+}
 
 export default App;
