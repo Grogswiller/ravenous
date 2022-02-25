5e729178d71de601b4243edd851f62e6564c0931
Grogswiller <ryanchipeniuk@gmail.com>
2022-02-24 20:16:19 -0700

    almost finished, search button not functioning

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,80 +1,82 @@
 import React from 'react';
 import './SearchBar.css';
 
-
 class SearchBar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            term:'',
-            location:'',
-            sortBy:'best_match'
-        }
-        this.sortByOptions = {
-            'Best Match': 'best_match',
-            'Highest Rated': 'rating',
-            'Most Reviewed': 'review_count'
-        }
-        this.handleTermChange = this.handleTermChange.bind(this);
-        this.handleLocationChange = this.handleLocationChange.bind(this);
-        this.handleSearch = this.handleSearch.bind(this);
-    }
+  constructor(props) {
+    super(props);
 
-    getSortByClass(sortByOption) {
-        if(sortByOption = this.state) {
-            return 'active';
-        } else {
-            return '';
-        }
-    }
+    this.state = {
+      term: '',
+      location: '',
+      sortBy: 'best_match'
+    };
 
-    handleSortByChange(sortByOption) {
-        this.setState({sortBy: sortByOption});
-    }
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleSortByChange = this.handleSortByChange.bind(this);
 
-    handleTermChange(event) {
-        this.setState({term: event.target.value});
-    }
+    this.sortByOptions = {
+      'Best Match': 'best_match',
+      'Highest Rated': 'rating',
+      'Most Reviewed': 'review_count'
+    };
+  }
 
-    handleLocationChange(event) {
-        this.setState({location: event.target.value}); 
+  getSortByClass(sortByOption) {
+    if (this.state.sortBy === sortByOption) {
+      return 'active';
     }
+    return '';
+  }
 
-    handleSearch(event) {
-        this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+  handleSortByChange(sortByOption) {
+    this.setState({sortBy: sortByOption});
+  }
 
-        event.preventDefault();
-    }
+  handleTermChange(event) {
+    this.setState({term: event.target.value});
+  }
 
-    renderSortByOptions() {
-        return Object.keys(this.sortByOptions).map(sortByOption => {
-            let sortByOptionValue = this.sortByOptions[sortByOption];
-            return (<li onClick={this.handleSortByChange.bind(this, sortByOptionValue)} 
-                        className={this.getSortByClass(sortByOptionValue)} 
-                        key={sortByOptionValue}>
-                        {sortByOption}
-                    </li>)
-        });
-    }
+  handleLocationChange(event) {
+    this.setState({location: event.target.value});
+  }
 
-    render() {  
-        return (
-            <div className="SearchBar">
-                <div className="SearchBar-sort-options">
-                    <ul>
-                    {this.renderSortByOptions()}    
-                    </ul>
-                </div>
-                <div className="SearchBar-fields">
-                    <input onChange={this.handleTermChange.bind(this)} placeholder="Search Businesses" />
-                    <input onChange={this.handleLocationChange.bind(this)} placeholder="Where?" />
-                </div>
-                <div className="SearchBar-submit">
-                    <a onClick={this.handleSearch}>Let's Go</a>
-                </div>
-            </div>
-        )
-    }
+  handleSearch(event) {
+    this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+
+    event.preventDefault();
+  }
+
+  renderSortByOptions() {
+    return Object.keys(this.sortByOptions).map(sortByOption => {
+      let sortByOptionValue = this.sortByOptions[sortByOption];
+      return (<li className={this.getSortByClass(sortByOptionValue)}
+                  key={sortByOptionValue}
+                  onClick={this.handleSortByChange.bind(this, sortByOptionValue)}>
+                {sortByOption}
+             </li>);
+    });
+  }
+
+  render() {
+    return (
+      <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+          <ul>
+            {this.renderSortByOptions()}
+          </ul>
+        </div>
+        <div className="SearchBar-fields">
+          <input placeholder="Search Businesses" onChange={this.handleTermChange} />
+          <input placeholder="Where?" onChange={this.handleLocationChange}/>
+        </div>
+        <div className="SearchBar-submit">
+          <a onClick={this.handleSearch}>Let's Go</a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,7 +1,6 @@
 const apiKey = 'rc2TsQ_ClkScNdg3Do04fAW8EcyOC3mBocmbj_ojn09O2y-cQ6fY4gH8o3yrBj0tpVKTjE18CkeZDj-SV66Z-nNE7cA9dXVgjPjLwI4Deg4yI0OyCze9pLaLPr8KYnYx';
 
 const Yelp = {
-
     search(term, location, sortBy) {
         return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
             headers: {
@@ -13,18 +12,20 @@ const Yelp = {
             if(jsonResponse.businesses) {
                 return jsonResponse.businesses.map(business => {
                     return {
-                        id: business.props.id,
-                        imageSrc: business.props.imageSrc,
-                        name: business.props.name,
-                        address: business.props.address,
-                        city: business.props.city,
-                        state: business.props.state,
-                        zipCode: business.props.zipCode,
-                        category: business.props.category,
-                        rating: business.props.rating,
-                        reviewCount: business.props.reviewCount
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories[0].title,
+                        rating: business.rating,
+                        reviewCount: business.review_count
                     };
                 });
             }
     });
 }};
+
+export default Yelp;
